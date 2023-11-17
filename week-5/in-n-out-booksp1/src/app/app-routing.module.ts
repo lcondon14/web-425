8eb2ef125980de50903b819d539ca6b2340db864
lcondon14 <122494018+lcondon14@users.noreply.github.com>
2023-11-17 11:30:34 -0600

    week-5 part 1

---
+++ week-5/in-n-out-booksp1/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+// Title: app-routing.module.ts
+// Name: Laurel Condon
+// Date: 16 Nov 2023
+// Description: app routing module for in n out books
+
+import { BookListComponent } from './book-list/book-list.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: BookListComponent },
+  { path: 'book-list', component: BookListComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
